7243158805e1506c9cd1ef72c870dce3c79baf45
robo38pro@outlook.com <robo38pro@outlook.com>
2019-03-17 12:45:26 +0500

    popen fix

--- popen.py
+++ popen.py
@@ -1,5 +1,5 @@
 import os
 m=[2,3,4,17,27,22,14,15,18,23,24]
 for n in m:
-    os.system('echo '+n+' > /sys/class/gpio/export')
-    os.system('echo "out" > /sys/class/gpio/gpio'+n+'/direction')
+    os.system('echo '+str(n)+' > /sys/class/gpio/export')
+    os.system('echo "out" > /sys/class/gpio/gpio'+str(n)+'/direction')
