5478b93f7ee17a34134822ca95553b4f239ba9a0
robo38pro@outlook.com <robo38pro@outlook.com>
2019-03-17 17:18:19 +0500

    fdyt

--- CV_EV3_BT.py
+++ CV_EV3_BT.py
@@ -217,7 +217,7 @@ if hmm == 'n':
                         template=cv.cvtColor(cv.imread(str(o)+'.png'),cv.COLOR_BGR2GRAY)
                         resu=cv.matchTemplate(roImg,template,cv.TM_CCOEFF_NORMED)
                         minv,maxv,minl,maxl=cv.minMaxLoc(resu)
-                        if maxv>0.6:
+                        if maxv>0.8:
                             print('!!!    '+cn+' ',int(o))
             break
             if cv.waitKey(1)== ord('q'):
