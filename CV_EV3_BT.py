46459353ad6a540b14cf3a8e3a5d5b31b8cade56
robo38pro@outlook.com <robo38pro@outlook.com>
2019-03-17 15:06:55 +0500

    np?

--- CV_EV3_BT.py
+++ CV_EV3_BT.py
@@ -211,7 +211,7 @@ if hmm == 'n':
                 #cv.imshow('detect',roImg)
                 #cv.imshow('n',n1)
                 #cv.imshow('n2',n7)
-                roImg=np.unit8(roImg)
+                roImg.astype(np.unit8)
 
             for o in range(1,10):
                 template=cv.imread(str(o)+'.png')
